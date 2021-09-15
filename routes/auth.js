da0b9fdc588113069e4b0a06bc3f8c2100430eb6
Vihaan Verma <vverma270705@gmail.com>
2021-09-15 13:37:16 +0530

    Last commit

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,8 @@ const router = Router();
 router.route("/").get(decode, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
+    if (!user)
+      return res.status(404).json({ errors: [{ msg: "User Not Found!" }] });
     res.json({ user });
   } catch (e) {
     res.status(400).json({ errors: [{ msg: e }] });
