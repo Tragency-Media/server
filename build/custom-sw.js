2bd5bc62cd403c31a56a73494384134fe649759b
Vihaan Verma <vverma270705@gmail.com>
2021-10-08 09:25:02 +0530

    bug notification fixed

--- build/custom-sw.js
+++ build/custom-sw.js
@@ -5,6 +5,22 @@ self.addEventListener("push", (event) => {
   const options = {
     body: data.body,
     icon: data.icon,
+    vibrate: [300, 100, 400],
+    data: { url: data.url },
+    action: [{ type: "open_url" }],
   };
   event.waitUntil(self.registration.showNotification(data.title, options));
 });
+self.addEventListener(
+  "notificationclick",
+  function (event) {
+    switch (event.action) {
+      case "open_url":
+        clients.openWindow(event.notification.data.url); //which we got from above
+        break;
+      default:
+        break;
+    }
+  },
+  false
+);
