6548f4257122594569faf8c9499113e4ab0f35fe
Vihaan Verma <vverma270705@gmail.com>
2021-10-08 11:13:54 +0530

    notification onclick

--- build/custom-sw.js
+++ build/custom-sw.js
@@ -11,16 +11,47 @@ self.addEventListener("push", (event) => {
   };
   event.waitUntil(self.registration.showNotification(data.title, options));
 });
-self.addEventListener(
-  "notificationclick",
-  function (event) {
-    switch (event.action) {
-      case "open_url":
-        clients.openWindow(event.notification.data.url); //which we got from above
-        break;
-      default:
-        break;
-    }
-  },
-  false
-);
+// self.addEventListener("notificationclick", (event) => {
+//   event.waitUntil(
+//     clients
+//       .matchAll({ includeUncontrolled: true, type: "window" })
+//       .then((windowClients) => {
+//         // Check if there is already a window/tab open with the target URL
+//         for (var i = 0; i < windowClients.length; i++) {
+//           var client = windowClients[i];
+//           // If so, just focus it.
+//           if (client.url === url) {
+//             return client.focus();
+//           }
+//         }
+//         // If not, then open the target URL in a new window/tab.
+//         if (clients.openWindow) {
+//           return clients.openWindow(url);
+//         }
+//       })
+//   );
+// });
+
+addEventListener("notificationclick", (e) => {
+  e.notification.close(); // Android needs explicit close.
+  e.waitUntil(
+    clients
+      .matchAll({ includeUncontrolled: true, type: "window" })
+      .then((clientsArr) => {
+        // If a Window tab matching the targeted URL already exists, focus that;
+        const hadWindowToFocus = clientsArr.some((windowClient) =>
+          windowClient.url ===
+          `${self.location.origin + e.notification.data.url}`
+            ? (windowClient.focus(), true)
+            : false
+        );
+        // Otherwise, open a new tab to the applicable URL and focus it.
+        if (!hadWindowToFocus)
+          clients
+            .openWindow(`${self.location.origin + e.notification.data.url}`)
+            .then((windowClient) =>
+              windowClient ? windowClient.focus() : null
+            );
+      })
+  );
+});
